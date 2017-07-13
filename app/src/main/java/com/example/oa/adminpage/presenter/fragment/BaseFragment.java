b7a16acf68996d0dc2c95cf396c6b3d8ba5c34f7
Phoenix <huuloc.phan@gmail.com>
2017-07-13 20:52:24 +0700

    List menu

--- app/src/main/java/com/example/oa/adminpage/presenter/fragment/BaseFragment.java
+++ app/src/main/java/com/example/oa/adminpage/presenter/fragment/BaseFragment.java
@@ -2,6 +2,8 @@ package com.example.oa.adminpage.presenter.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.example.oa.adminpage.presenter.di.HasComponent;
+
 /**
  * Created by Phoenix on 5/25/17.
  */
@@ -9,4 +11,11 @@ import android.support.v4.app.Fragment;
 public abstract class BaseFragment extends Fragment {
     public static final String TYPE = "type_fragment";
 
+    /**
+     * Gets a component for dependency injection by its type.
+     */
+    @SuppressWarnings("unchecked")
+    protected <C> C getComponent(Class<C> componentType) {
+        return componentType.cast(((HasComponent<C>) getActivity()).getComponent());
+    }
 }
